1471c63f66ee175d46274df45887e5e1a5bc4bb6
Carson Comar <Carsonccomar@gmail.com>
2024-07-24 22:23:38 -0500

    Fix syntax error in App.jsx

--- Contact-List/src/App.jsx
+++ Contact-List/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import ContactList from './components/ContactList.jsx';
-import SelectedContact from './components/SelectedContact.jsx';
+import ContactList from './components/ContactList';
+import SelectedContact from './components/SelectedContact';
 
 export default function App() {
   const [selectedContactId, setSelectedContactId] = useState(null);
