ce1e1270c94097fea40c5ac76a26b1993e8f89c0
Carson Comar <Carsonccomar@gmail.com>
2024-07-24 21:43:19 -0500

    Fix missing closing brace and syntax errors

--- Contact-List/src/components/ContactRow.jsx
+++ Contact-List/src/components/ContactRow.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react'
 export default function ContactRow({ contact, setSelectedContactId }) {
   return (
     <tr
       onClick={() => {
-        setSelectedContactId(contact.id);
+        setSelectedContactId(contact.id)
       }}
     >
       <td>{contact.name}</td>
       <td>{contact.email}</td>
       <td>{contact.phone}</td>
     </tr>
-  );
+  )
 }
